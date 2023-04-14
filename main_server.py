8a0ac5bdef0c534472e02d674f3d9e8bfee6ac4e
SFAS0 <sfas7@mail.ru>
2023-04-14 12:51:45 +0300

    совмещаю ветки

--- main_server.py
+++ main_server.py
@@ -1,14 +1,23 @@
 from flask import Flask, url_for, request, redirect, render_template
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from wtforms.validators import DataRequired
 
 from data import db_session
 from data.users import User
 from data.history_of_requests import HistoryOfRequests
 from data.district import District
 from data.geo_regions import Regions
+from forms.user_registration import RegisterForm
+from forms.user_login import LoginForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class MainPage(FlaskForm):
+    submit = SubmitField('regions')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,8 +29,40 @@ def main():
 
 @app.route('/regions/<reg_name>')
 def roots(reg_name):
-    param = {'regname': reg_name, 'regimg': 'https://static-maps.yandex.ru/1.x/?ll=30.458673,59.898065&size=650,450&z=7&l=map'}
-    return render_template('info_regs.html', **param)
+    return f'{reg_name}'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        user = User(
+            login=form.login.data,
+            password=form.password.data
+        )
+        user.set_password(form.password.data)
+        db.add(user)
+        db.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success', methods=['GET', 'POST'])
+def success():
+    # не готово
+    render_template('login.html', title='Авторизация', form={})
 
 
 if __name__ == '__main__':
